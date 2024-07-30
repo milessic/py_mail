316eda01a9a420b13fa9f22b98de9eac6d86ffa2
Miłosz Jura <milosz.m.jura@gmail.com>
2024-07-30 17:07:40 +0200

    Added more headless settings, enhacements on CLI UX

--- pymail.py
+++ pymail.py
@@ -1,14 +1,25 @@
 from mailing import MailClient
 import sys
 import os
+from tabulate import tabulate
 creds= None
 
 help_msg = """PyMail, simple tool for SMTP mailing sending using gmail
 
 pymail [To] [Subject] [Content]
 
---interface -i  - opens command line interface
+--interface  -i  - opens command line interface
+             -f  - use recipient from favorites
+--list-favorites - list all favorites
+--silent         - mailing client does not print anything execpt errors, works only for headless
+
+example:
+    pymail -fdarling "I will be later" "Hello my darling!\n\nI will be home little later\nWith Love\nM"
+    pymail some@mail.com "Test mail" "This is a test." --silent
+
+    pymail -i < opens Command Line Interface 
 """
+is_silent = not "--silent" in sys.argv
 
 def clear():
     os.system("cls" if os.name=="nt" else "clear")
@@ -34,7 +45,7 @@ except FileNotFoundError:
     print("\33[38;1;41mCould not locate 'credentials.creds'! Create such file under /home/{user}/.pymail.conf and insert your credentials in this way: 'my@mail.com,Password1'\33[0m")
     start = False
 
-if "--help" in sys.argv:
+if "--help" in sys.argv or len(sys.argv) == 1:
     print(help_msg)
     start = False
 
@@ -56,11 +67,11 @@ if start:
         while start:
             try:
                 clear()
-                print(f"\33[3mLogged as {m.credentials.login}, {m.get_number_of_mails()}\33[0m")
+                print(f"\33[3mLogged as {m.credentials.login}, Inbox: {m.get_number_of_mails()}\33[0m")
                 print("- NEW MAIL 0\n- SHOW FAVORITES 1\n- ADD TO FAVORITES 2\n- REMOVE FROM FAVORITES 3\n- INBOX 4\n- EXIT 9")
                 inp = input(">>> ")
                 match inp.upper():
-                    case "0" | "NEW MAIL":
+                    case "0" | "NEW" | "NEW MAIL":
                         m.send_mail()
                     case "1" | "SHOW FAVORITES":
                         m.show_favorites()
@@ -70,7 +81,7 @@ if start:
                         m.remove_from_favorites()
                     case "4" | "INBOX":
                         m.show_inbox()
-                    case "9":
+                    case "9" | "EXIT":
                         break
                     case _:
                         print("Not supported input")
@@ -78,8 +89,20 @@ if start:
                 clear()
             except (KeyboardInterrupt, EOFError):
                 break
+    elif "--list-favorites" in sys.argv:
+        m = MailClient(creds, config, initialize_mail=False, silent=is_silent)
+        data = m._fetch_all_favorites()
+        print(tabulate(data, headers="keys"))
     else:
-        m = MailClient(creds, config, silent=True)
-        msg = m._setup_message(sys.argv[1], sys.argv[2], sys.argv[3])
-        m._send_mail(msg)
+        m = MailClient(creds, config, silent=is_silent)
+        try:
+            # set to
+            if sys.argv[1].startswith("-f"):
+                recipient = m._fetch_from_favorites(sys.argv[1][2:])
+            else:
+                recipient = sys.argv[1]
+            msg = m._setup_message(to=recipient, subject=sys.argv[2], content=sys.argv[3])
+            m._send_mail(msg)
+        except IndexError:
+            print("Some arguments are missing! use --help!")
 
